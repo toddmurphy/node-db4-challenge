fe33a0bfdddaa90d93a327049d0152145ae9500a
toddmurphy <toddmurphy93@gmail.com>
2020-01-23 22:44:50 -0400

    server get up and running

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const server = require('./server');
 
 const PORT = process.env.PORT;
 
-// server.listen(PORT, () => {
-//   console.log(`Server running on ${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
